db3f26769d7f912c2873600074ca967229f4c4dc
Mohammad Ali <aliparkour83@gmail.com>
2023-11-20 12:37:52 +0330

    Customer schema has been created

---
+++ models/Customer.js
@@ -0,0 +1,40 @@
+import { model, models, Schema } from "mongoose";
+
+const customerSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 1,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minLength: 1,
+    },
+    email: {
+        type: String,
+        required: true,
+        minLength: 1,
+    },
+    phoneNumber: String,
+    address: String,
+    postalCode: Number,
+    data: Date,
+    products: {
+        type: Array,
+        default: [],
+    },
+    createdAt: {
+        type: Date,
+        default: () => Date.now(),
+        immutable: true
+    },
+    updatedAt: {
+        type: Date,
+        default: () => Date.now(),
+    }
+});
+
+const Customer = models.Customer() || model("Customer", customerSchema);
+
+export default Customer;
