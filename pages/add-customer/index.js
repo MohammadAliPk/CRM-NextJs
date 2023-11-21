321b61b046d9563b101eb030e8eafc3176efa8b6
Mohammad Ali <aliparkour83@gmail.com>
2023-11-21 21:37:26 +0330

    Add Customer frontend has been created

---
+++ pages/add-customer/index.js
@@ -0,0 +1,10 @@
+import AddCustomer from '@/components/template/AddCustomer';
+import React from 'react';
+
+const index = () => {
+    return (
+        <AddCustomer />
+    );
+};
+
+export default index;
