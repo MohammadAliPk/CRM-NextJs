164a1cabaa01ced7fb567526da460a0a8c1735aa
Mohammad Ali <aliparkour83@gmail.com>
2023-11-24 18:08:25 +0330

    button bug has been fixed

--- components/template/AddCustomer.js
+++ components/template/AddCustomer.js
@@ -3,6 +3,8 @@ import Form from '../module/Form';
 import { useRouter } from 'next/router';
 
 const AddCustomer = () => {
+    const [isSaving, setIsSaving] = useState(false);
+
     const [form, setForm] = useState({
         name: '',
         lastName: '',
@@ -17,15 +19,20 @@ const AddCustomer = () => {
     const router = useRouter();
 
     const saveHandler = async () => {
-        const res = await fetch("/api/customer", {
-            method: 'POST',
-            body: JSON.stringify({ data: form }),
-            headers: { 'Content-Type': 'application/json' }
-        })
-        const data = await res.json();
-        console.log(data);
-        if (data.status === 'Success') {
-            router.push("/")
+        if (!isSaving) {
+            setIsSaving(true);
+            const res = await fetch("/api/customer", {
+                method: 'POST',
+                body: JSON.stringify({ data: form }),
+                headers: { 'Content-Type': 'application/json' }
+            })
+            const data = await res.json();
+            console.log(data);
+            if (data.status === 'Success') {
+                router.push("/")
+            } else if (data.status === 'Failed') {
+                setIsSaving(false);
+            }
         }
     }
 
@@ -48,8 +55,9 @@ const AddCustomer = () => {
             <Form form={form} setForm={setForm} />
             <div className='customer-page__buttons'>
                 <button className='first' onClick={cancelHandler}>Cancel</button>
-                <button className='second' onClick={saveHandler}>Save</button>
-            </div>
+                <button className='second' onClick={saveHandler} >
+                    {isSaving ? 'Saving...' : 'Save'}
+                </button>            </div>
         </div>
     );
 };
