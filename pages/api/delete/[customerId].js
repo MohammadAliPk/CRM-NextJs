0647118fc881dc934c8ef53950477c2d1d149788
Mohammad Ali <aliparkour83@gmail.com>
2023-11-25 18:25:23 +0330

    delete customer api has been created

---
+++ pages/api/delete/[customerId].js
@@ -0,0 +1,31 @@
+import Customer from "@/models/Customer";
+import connectDB from "@/utils/connectDB";
+
+export default async function handler(req, res) {
+    try {
+        connectDB();
+    } catch (err) {
+        console.error("Error connecting to database");
+        res.status(500).json({
+            status: "Failed",
+            message: "Error connecting to database"
+        })
+    }
+    if (req.method === "DELETE") {
+        const id = req.query.customerId;
+
+        try {
+            await Customer.deleteOne({ _id: id });
+            res.status(200).json({
+                status: "Success",
+                message: "Customer deleted successfully"
+            })
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).json({
+                status: "Failed",
+                message: "Error deleting customer"
+            })
+        }
+    }
+}
