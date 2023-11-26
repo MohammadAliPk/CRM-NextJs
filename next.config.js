3716729402d9127068aa864bbd6d908411cc760a
Mohammad Ali <aliparkour83@gmail.com>
2023-11-26 23:49:39 +0330

    Made the project PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+
+
+module.exports = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig);
