901728e66e914306812fec21426f09f071b97dbd
Mohammad Ali <aliparkour83@gmail.com>
2023-11-26 19:40:38 +0330

    edit page has been created

---
+++ pages/edit/[customerId].js
@@ -0,0 +1,22 @@
+import CustomerEditPage from '@/components/template/CustomerEditPage';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+
+const Index = () => {
+    const [data, setData] = useState(null);
+    const router = useRouter();
+    const { query: { customerId }, isReady } = router;
+
+    useEffect(() => {
+        if (isReady) {
+            fetch(`/api/customer/${customerId}`).then(res => res.json()).then(data => setData(data.data));
+        }
+    }, [isReady])
+    if (data) {
+        return (
+            <CustomerEditPage data={data} id={customerId} />
+        );
+    };
+};
+
+export default Index;
